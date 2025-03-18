04335dff00666946fa602a2e69d125947b546df0
unknown <dinbiton100@gmail.com>
2025-03-18 22:21:28 +0200

    Services

---
+++ OkooraAssignment/OkooraAssignment/Services/RatePrinterService.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+namespace OkooraAssignment.Services
+{
+    public class RatePrinterService
+    {
+        public RatePrinterService() {}
+
+        public async Task<Dictionary<string, decimal>?> GetSavedRatesFromFile()
+        {
+            try
+            {
+                string filePath = Path.Combine("DB", "exchange_rates.json");
+                if (!System.IO.File.Exists(filePath))
+                {
+                    return null; // No saved rates found
+                }
+
+                string json = await System.IO.File.ReadAllTextAsync(filePath);
+                Dictionary<string, decimal>? savedRates = JsonSerializer.Deserialize<Dictionary<string, decimal>>(json);
+
+                return savedRates;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading saved rates from file: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
