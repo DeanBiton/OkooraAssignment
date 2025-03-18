c12162ed73c43ab90d53e080cd0679f8bbabed50
unknown <dinbiton100@gmail.com>
2025-03-18 21:08:41 +0200

    Models

---
+++ OkooraAssignment/OkooraAssignment/Models/FxRateModels/FxRateRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace OkooraAssignment.Models.FxRateModels
+{
+    public class FxRateRequest
+    {
+        public string requestURL { get; private set; }
+
+        public FxRateRequest(string baseCurrency, List<string> currencies) 
+        {
+            requestURL = $"https://api.fxratesapi.com/latest?base={baseCurrency}&currencies={string.Join(',', currencies)}&resolution=1m&amount=1&places=6&format=json";
+        }
+    }
+}
---
+++ OkooraAssignment/OkooraAssignment/Models/FxRateModels/FxRatesResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OkooraAssignment.Models.FxRateModels
+{
+    public class FxRatesResponse
+    {
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+
+        [JsonPropertyName("terms")]
+        public string Terms { get; set; }
+
+        [JsonPropertyName("privacy")]
+        public string Privacy { get; set; }
+
+        [JsonPropertyName("timestamp")]
+        public long Timestamp { get; set; }
+
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("base")]
+        public string BaseCurrency { get; set; }
+
+        [JsonPropertyName("rates")]
+        public Dictionary<string, decimal> Rates { get; set; }
+    }
+}
